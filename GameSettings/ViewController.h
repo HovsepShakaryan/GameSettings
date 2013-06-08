864934c9bc87905f4c94ea70b4624fec39f3ab90
HovsepShakaryan <hovsep.shakaryan@gmail.com>
2013-06-08 12:38:17 +0400

    Initial Commit

---
+++ GameSettings/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GameSettings
+//
+//  Created by Hovsep Shakarayn on 08/06/2013.
+//  Copyright (c) 2013 Hovsep Shakarayn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GameSettings/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GameSettings
+//
+//  Created by Hovsep Shakarayn on 08/06/2013.
+//  Copyright (c) 2013 Hovsep Shakarayn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
